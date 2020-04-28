8d132a030f5e823a6d12dc1d613f588bbfd7db02
Julia Yusupova <cashleva.iulia@gmail.com>
2020-04-28 18:50:54 +0300

    Added Dropdown with humans

---
+++ src/blocks/dropdown-room/dropdown-room.js
@@ -0,0 +1,77 @@
+// let clear = document.getElementById('clear');
+// let apply = document.getElementById('apply');
+// let guests = document.getElementById('guests');
+// let arrowDown = document.getElementById('arrowDown');
+// let list = document.getElementById ('dropdownHide');
+// let container = document.getElementById ('container');
+// let result = document.querySelectorAll('.blockDropdownMain .pCounter');
+// let minus = document.querySelectorAll('.blockDropdownMain button.buttonMinus');
+// let plus = document.querySelectorAll('.blockDropdownMain button.buttonPlus');
+
+// arrowDown.onclick = OpenList;
+// apply.onclick = Result;
+// clear.onclick = Clear;
+// minus[0].onclick = minus[1].onclick = minus[2].onclick = CounterMinus;
+// plus[0].onclick = plus[1].onclick = plus[2].onclick = CounterPlus;
+
+// for (let i = 0; i < 3; i++){
+//   if(result[i] > 0){
+//     clear.classList.remove('buttonMinus_type_inactive');   
+//   }
+// }
+
+// function OpenList() {
+//   list.classList.toggle('dropdownHide');
+//   container.classList.toggle('removeUnderline');
+// }
+
+// function CounterPlus (e) {
+//   let p = e.target.previousSibling;
+//   let value = Number(p.innerText);
+//   value ++;
+//   p.innerText = value;
+//     if (value > 0){
+//       p.previousSibling.classList.remove('buttonMinus_type_inactive');
+//     }
+// }
+
+// function CounterMinus (e) {
+//   let p = e.target.nextSibling;
+//   let value = Number(p.innerText);
+//   if (value > 0){
+//   value --;
+//   p.innerText = value;
+//     if (value === 0) {
+//       e.target.classList.add('buttonMinus_type_inactive');
+//     }
+//   }
+// }
+
+// function Result() {
+//     let sum = 0;
+//   for (let i = 0; i < 3; i++){
+//     sum = sum + Number(result[i].innerText)   
+//   }
+//   if (sum > 0){
+//     switch (sum) {
+//       case 1:
+//         sum  += ' гость';
+//       break;
+//       case 2 || 3 || 4:
+//         sum  += ' гостя';
+//       break;
+//       default:
+//         sum  += ' гостей';
+//       break;
+//       }
+//     guests.innerText = sum;
+//   }
+// }
+
+// function Clear() {
+//   guests.innerText = "Сколько гостей";
+//   for (let i = 0; i < 3; i++){
+//     result[i].innerText = 0;
+//     minus[i].classList.add('buttonMinus_type_inactive');   
+//   }
+// }
--- src/blocks/dropdown/dropdown.js
+++ src/blocks/dropdown/dropdown.js
@@ -1,2 +1,76 @@
-import './dropdown__counter'
+let clear = document.getElementById('clear');
+let apply = document.getElementById('apply');
+let guests = document.getElementById('guests');
+let arrowDown = document.getElementById('arrowDown');
+let list = document.getElementById ('dropdownHide');
+let container = document.getElementById ('container');
+let result = document.querySelectorAll('.blockDropdownMain .pCounter');
+let minus = document.querySelectorAll('.blockDropdownMain button.buttonMinus');
+let plus = document.querySelectorAll('.blockDropdownMain button.buttonPlus');
 
+arrowDown.onclick = OpenList;
+apply.onclick = Result;
+clear.onclick = Clear;
+minus[0].onclick = minus[1].onclick = minus[2].onclick = CounterMinus;
+plus[0].onclick = plus[1].onclick = plus[2].onclick = CounterPlus;
+
+function OpenList() {
+  list.classList.toggle('dropdownHide');
+  container.classList.toggle('removeUnderline');
+}
+
+function CounterPlus (e) {
+  let p = e.target.previousSibling;
+  let value = Number(p.innerText);
+  value ++;
+  p.innerText = value;
+    if (value > 0){
+      p.previousSibling.classList.remove('buttonMinus_type_inactive');
+      clear.classList.remove('clearHide');
+    }
+}
+
+function CounterMinus (e) {
+  let p = e.target.nextSibling;
+  let value = Number(p.innerText);
+  if ((result[0].innerText && result[1].innerText) && result[2].innerText === '0'){
+    clear.classList.add('clearHide');
+  }
+  console.log(result[0].innerText)
+  if (value > 0){
+  value --;
+  p.innerText = value;
+    if (value === 0) {
+      e.target.classList.add('buttonMinus_type_inactive');
+    }
+  }
+}
+
+function Result() {
+    let sum = 0;
+  for (let i = 0; i < 3; i++){
+    sum = sum + Number(result[i].innerText)   
+  }
+  if (sum > 0){
+    switch (sum) {
+      case 1:
+        sum  += ' гость';
+      break;
+      case 2 || 3 || 4:
+        sum  += ' гостя';
+      break;
+      default:
+        sum  += ' гостей';
+      break;
+      }
+    guests.innerText = sum;
+  }
+}
+
+function Clear() {
+  guests.innerText = "Сколько гостей";
+  for (let i = 0; i < 3; i++){
+    result[i].innerText = 0;
+    minus[i].classList.add('buttonMinus_type_inactive');   
+  }
+}

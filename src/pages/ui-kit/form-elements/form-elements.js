6e048cf9c4625a18d71630f6941f11a54bb6c917
Julia Yusupova <cashleva.iulia@gmail.com>
2020-05-04 22:59:53 +0300

    Added cards with rooms

--- src/pages/ui-kit/form-elements/form-elements.js
+++ src/pages/ui-kit/form-elements/form-elements.js
@@ -2,6 +2,5 @@ import "./form-elements.scss"
 import '../../../blocks/like-button/like-button.js'
 import '../../../blocks/expandable-checkbox-list/expandable-checkbox-list.js'
 import '../../../blocks/range-slider/range-slider.js'
-import '../../../blocks/rate-button/rate-button.js'
 import '../../../blocks/dropdown/dropdown.js'
 import '../../../blocks/dropdown-room/dropdown-room.js'

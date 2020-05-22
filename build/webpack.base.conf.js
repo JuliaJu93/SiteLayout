dd99cb9788dfc6bd2035dfc6628a85d93bacc71f
Julia Yusupova <cashleva.iulia@gmail.com>
2020-05-22 18:38:01 +0300

    fixed links

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -73,7 +73,6 @@ module.exports = {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              publicPuth: '/'
             },
           },
         ],

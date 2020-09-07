d4205929c097539eb10d02b137700e0b55f2516f
Julia Yusupova <cashleva.iulia@gmail.com>
2020-09-07 22:57:18 +0300

    fixed links

--- src/pages/website-pages/room-details/room-details.js
+++ src/pages/website-pages/room-details/room-details.js
@@ -3,4 +3,4 @@ import '../../../blocks/calendar/calendar';
 import '../../../blocks/calendar/calendar-interval';
 import '../../../blocks/calendar/calendar-apply';
 import '../../../blocks/calendar/calendar-open';
-import '../../../blocks/dropdown/dropdown';
+import '../../../elements/dropdown/dropdown';
